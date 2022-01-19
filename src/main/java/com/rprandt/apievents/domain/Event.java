dd24f9c0603dc5dc6a457ac51c7d1c0285dc5657
Richard Prandt <rprandt.contato@gmail.com>
2022-01-19 10:42:37 -0300

    address to event added

--- src/main/java/com/rprandt/apievents/domain/Event.java
+++ src/main/java/com/rprandt/apievents/domain/Event.java
@@ -1,11 +1,13 @@
 package com.rprandt.apievents.domain;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.rprandt.apievents.dto.Address;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,12 +17,14 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Event{
-
+public class Event implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	private String id;
 	private String name;
 	@JsonFormat(pattern = "dd-MM-yyyy")
 	private Date date;
+	private Address addressEvent;
 	
 }
---
+++ src/main/java/com/rprandt/apievents/dto/Address.java
@@ -0,0 +1,20 @@
+package com.rprandt.apievents.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String street;
+	private String city;
+	private String state;
+	private String postalCode;
+
+}
